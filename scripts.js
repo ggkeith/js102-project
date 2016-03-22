51221aec92cbd66e1c78b9792069df089d1d9918
George Keith <george@awkwardlybrilliant.com>
2016-03-22 10:31:28 -0500

    add animal Object

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
 //Use this file to implement Part One of your project
 
+var animal = {};
 
+animal.username = "Pickles";
+console.log(animal.username);
+
+animal["tagline"] = "I love muffins!";
+console.log(animal["tagline"]);
+
+var noises = [];
+animal[noises] = noises;
+console.log(animal);
+
+for(var key in animal) {
+	
+}
