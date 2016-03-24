e171803042effe8dfc5357f2e058cb90df8df0a1
George Keith <ggkeithv@gmail.com>
2016-03-23 20:16:28 -0500

    removed empty array and console.log statements

--- scripts.js
+++ scripts.js
@@ -3,15 +3,22 @@
 var animal = {};
 
 animal.username = "Pickles";
-console.log(animal.username);
 
 animal["tagline"] = "I love muffins!";
-console.log(animal["tagline"]);
 
 var noises = [];
-animal[noises] = noises;
+animal.noises = noises;
+
 console.log(animal);
 
+var count = 0;
 for(var key in animal) {
-	
+	count++;
+	if (key === "username") {
+		console.log("Hi my name is " + animal[key]);
+	} else if (key === "tagline") {
+		console.log("I like to say " + animal[key]);
+	}
 }
+
+
