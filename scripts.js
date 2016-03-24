00a43e8c580f79477fb8cd4afeb8b4f7dab431b9
George Keith <ggkeithv@gmail.com>
2016-03-24 07:31:38 -0500

    almost finished with last part of Array exercise

--- scripts.js
+++ scripts.js
@@ -9,16 +9,63 @@ animal["tagline"] = "I love muffins!";
 var noises = [];
 animal.noises = noises;
 
-console.log(animal);
+//console.log(animal);
 
-var count = 0;
-for(var key in animal) {
-	count++;
-	if (key === "username") {
-		console.log("Hi my name is " + animal[key]);
-	} else if (key === "tagline") {
-		console.log("I like to say " + animal[key]);
-	}
-}
+/*var count = 0;
+for (var key in animal) {
+    count++;
+    if (key === "username") {
+        console.log("Hi my name is " + animal[key]);
+    } else if (key === "tagline") {
+        console.log("I like to say " + animal[key]);
+    }
+}*/
 
 
+var noiseArray = [];
+
+noiseArray.unshift("fart");
+noiseArray.push("neigh");
+
+noiseArray[3] = "burp";
+
+/*
+console.log(noiseArray);
+console.log(noiseArray.length);
+console.log(noiseArray[-1]);
+*/
+
+animal.noises = noiseArray;
+//console.log(animal);
+
+var animals = [];
+
+animals.animal = animal;
+
+var quackers = {
+    username: "Daffy Duck",
+    tagline: "Yipeee!",
+    noises: ["quack", "honk", "sneeze", "growl"]
+};
+
+animals[animal.length
+] = quackers;
+
+//console.log(animals);
+
+var dog = {
+    username: "Desi",
+    tagline: "I bark, therefore I am",
+    noises: ["bark", "whine", "grunt"]
+};
+
+var ferret = {
+	username: "Buster",
+	tagline: "I steal shit",
+	noises: ["squeak", "squeal", "nomnomnom"]
+};
+
+animals["dog"] = dog;
+animals["ferret"] = ferret;
+
+console.log(animals);
